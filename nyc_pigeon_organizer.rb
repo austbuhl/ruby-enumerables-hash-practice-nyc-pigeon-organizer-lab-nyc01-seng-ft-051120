ac7692aded9b2ceee977a7cb81a2141814a33085
Austin Buhler <austinbuhler@gmail.com>
2020-04-22 14:57:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,7 +6,7 @@ def nyc_pigeon_organizer(data)
         if new_hash[name] == nil
           new_hash[name] = {}
         end
-        
+
         if new_hash[name][color_gender_lives] == nil
           new_hash[name][color_gender_lives] = []
         end
